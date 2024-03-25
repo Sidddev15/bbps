401f2b603971a77ee558dfd5e1d73839dca90185
sidddev15 <siddharthpersonal@gmail.com>
2024-03-26 00:46:15 +0530

    Final Commit

--- js/loader.js
+++ js/loader.js
@@ -10,3 +10,16 @@ $(function () {
 
 // Initializing AOS
 AOS.init();
+
+
+Loader
+$(document).ready(function() {
+  // Show loader and fade in the screen when the page starts loading
+  $(window).on('load', function() {
+    $('.image-upload-loader').fadeIn('slow', function() {
+      // Fade out the loader after it has been shown
+      $(this).fadeOut('slow');
+    });
+    $('body').css('opacity', '1'); // Fade in the screen
+  });
+});
